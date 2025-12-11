cda2656e28bfd3a408de807489277b1df37784c3
anil1707 <yadavanil1707ax@gmail.com>
2025-12-11 23:47:10 +0530

    powerset or all subsequence problem

---
+++ array/subsequence/printAllSubseq.js
@@ -0,0 +1,26 @@
+console.log("power set/ all subsequence");
+
+let arr = [2, 3, 4];
+
+let subSeq = (arr, ans, temp, i) =>{
+    if(i >= arr.length){
+        if(temp.length)
+            ans.push(temp);
+        return;
+    }
+
+    // pick the value
+    subSeq(arr, ans, [...temp, arr[i]], i+1);
+
+    // not pick the value
+    subSeq(arr, ans, [...temp], i+1);
+}
+
+let allSubseq = (arr) =>{
+    let ans = [];
+    subSeq(arr, ans, [], 0);
+    return ans;
+}
+
+let ans = allSubseq(arr)
+console.log("All subsequences is: ", ans);
